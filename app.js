f9028f960787c30da4126a8553f1ebb7db9833a9
gavinning <ningyubo@gmail.com>
2016-01-26 18:47:07 +0800

    create

---
+++ app.js
@@ -0,0 +1,56 @@
+var $ = require('thenjs');
+var backup = require('./lib/backup');
+
+
+exports.start = function(url){
+    var config = require(url);
+    $(config.backupList)
+        .eachSeries(null, function(next, item){
+            backup.start(item, function(err){
+                err ?
+                    console.log(err.message):
+                    next();
+            })
+        })
+}
+
+
+// =========
+/*
+
+var backup = require('backup2');
+
+// eg1
+backup.start('./config');
+
+
+// eg2
+backup.watch('./config');
+
+
+// logs
+backup: 备份进程启动
+backup: 共两个任务：t1, t2
+backup: t1任务开始
+backup: 增量备份类型
+backup: 对比备份文件开始
+backup: 本次备份文件：
+a1,
+a2,
+a3,
+a4
+backup: 开始备份
+
+
+process start
+two task
+task 1 start | type: add
+.......................
+task 1 end
+
+task 2 start | type: full
+.......................
+task 2 end
+
+all task is end
+ */
